a6273bd366c01e73f3d396df59d22db0b9872ccc
lifeconsciousness <lifeconsciousness321@gmail.com>
2022-12-22 21:15:57 +0100

    guitar looks better

--- js/guitar.js
+++ js/guitar.js
@@ -1,14 +1,16 @@
 
 let path = document.querySelector("path")
-let pathLength = path.getTotalLength()+3000
+//let pathLength = path.getTotalLength()+3000
+let pathLength = path.getTotalLength()-3000
 
 path.style.strokeDasharray = pathLength + ' ' + pathLength
 path.style.strokeDashoffset = pathLength
 
 window.addEventListener("scroll", ()=>{
-    let scrollPercentage = ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight))*2.1;
+    let scrollPercentage = ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight))*5;
     
-    let drawLength = pathLength * scrollPercentage-500
+    //let drawLength = pathLength * scrollPercentage-500
+    let drawLength = pathLength * scrollPercentage+1450
 
     path.style.strokeDashoffset = pathLength - drawLength
 })
