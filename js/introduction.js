ae4f38e053ad9d8eb7c68a90effa4247746574fc
lifeconsciousness <lifeconsciousness321@gmail.com>
2022-12-25 20:34:34 +0100

    introduction section all done

--- js/introduction.js
+++ js/introduction.js
@@ -1,7 +1,9 @@
 const whiteStartAnimation = document.querySelector(".whiteStartAnimation"),
-      greetingsText = document.querySelector("#greetings")
-
+      greetingsText = document.querySelector("#greetings"),
+      exploreButton = document.querySelector("#explore")
+      
 window.addEventListener("load", ()=>{
     whiteStartAnimation.classList.add("animationItself")
     greetingsText.classList.add("greetingsAnimated")
+    exploreButton.classList.add("buttonAnimation")
 })
