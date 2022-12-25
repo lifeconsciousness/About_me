44b5cd9786d70198c99489aa53d1baf6dd5b0c56
lifeconsciousness <lifeconsciousness321@gmail.com>
2022-12-25 02:34:11 +0100

    concert image larger on click

--- js/songwriting.js
+++ js/songwriting.js
@@ -1,1 +1,23 @@
-let images = document.querySelectorAll(".largerImg")
+let images = document.querySelectorAll(".imageThatCanBeLarger"),
+    containers = document.querySelectorAll(".containerForImage"),
+    songwritingText = document.querySelector(".songwriting-text")
+
+let isEnabled = false;
+for(let i = 0; i < images.length; i++){
+    images[i].addEventListener("click", e =>{
+        e.target.classList.toggle("photo-from-concert")
+        e.target.classList.toggle("larger-img")
+
+        let parent = e.target.parentElement
+        parent.classList.toggle("img-container")
+
+        if(isEnabled){
+            songwritingText.style.display = "block"
+            isEnabled = false
+        } else{
+            songwritingText.style.display = "none"
+            isEnabled = true
+        }
+
+    })
+}
