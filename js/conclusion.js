f01067cfbf14c192c3d41a53988ca3881a3c204d
lifeconsciousness <lifeconsciousness321@gmail.com>
2022-12-26 23:13:42 +0100

    switching songs

--- js/conclusion.js
+++ js/conclusion.js
@@ -4,19 +4,35 @@ const waves = document.querySelectorAll(`.wave`),
 setTimeout(pauseWaves, 960)  
 audioPlayer.volume = 0.2
 
-audioPlayer.addEventListener("play", ()=>{
+audioPlayer.addEventListener(`play`, ()=>{
     playWaves()
 })
-audioPlayer.addEventListener("pause", ()=>{
+audioPlayer.addEventListener(`pause`, ()=>{
     pauseWaves()
 })
 
-
-
-
 function pauseWaves(){
-    waves.forEach(wave => wave.style.animationPlayState = "paused")
+    waves.forEach(wave => wave.style.animationPlayState = `paused`)
 }
 function playWaves(){
-    waves.forEach(wave => wave.style.animationPlayState = "running")
+    waves.forEach(wave => wave.style.animationPlayState = `running`)
+}
+
+
+const songOptions = document.querySelectorAll(`.song`)
+const songSource = document.querySelector(`.song-source`)
+const songsArr = [`./audio/insomnia.mp3`,`./audio/The_Regular_Person-song_about_nothing.mp3`,`light tide.mp3`]
+
+let prevSong = 1;
+
+for (let i = 0; i < songOptions.length; i++){
+    songOptions[i].addEventListener(`click`, (e)=>{
+        songSource.src = songsArr[i]
+        audioPlayer.load()
+
+        songOptions[prevSong].classList.remove("currentSong")
+        songOptions[i].classList.add("currentSong")
+
+        prevSong = i
+    })
 }
